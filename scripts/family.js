719afa0d5fcd629c8900357235933c697571e7ec
Ram Patil <ramcpatil27@gmail.com>
2026-06-19 20:06:14 +0530

    name

--- scripts/family.js
+++ scripts/family.js
@@ -14,7 +14,7 @@ const familylist = [
     number: "02",
     title: "It's My Father",
     description:
-      "My father was a very good and loving man.He always instilled in me values of positive thinking, humanity, and devotion.However, he left my side far too soon; consequently, I miss him deeply to this very day.It was through my father's companionship—and the values he imparted—that I was drawn toward the Warkari tradition.I inherited from him my passion for devotion to Lord Vitthal, for singing Abhangs, and for playing the Pakhawaj.Although my father is no longer physically with me today, his thoughts, his teachings, and his blessings remain with me forever.",
+      "My father Name is Chhagan (Rajendra) Laxman Patil. My father was a very good and loving man.He always instilled in me values of positive thinking, humanity, and devotion.However, he left my side far too soon; consequently, I miss him deeply to this very day.It was through my father's companionship—and the values he imparted—that I was drawn toward the Warkari tradition.I inherited from him my passion for devotion to Lord Vitthal, for singing Abhangs, and for playing the Pakhawaj.Although my father is no longer physically with me today, his thoughts, his teachings, and his blessings remain with me forever.",
     techStack: ["I'm Really Miss You Baba"],
     image: "assets/family/family2.webp",
   },
@@ -23,7 +23,7 @@ const familylist = [
     number: "03",
     title: "It's My Mother",
     description:
-      "Ever since my father passed away, my mother has single-handedly shouldered the entire responsibility of our home.She never once let me feel the absence of my father.My mother raised me by showering me with the love of both a mother and a father.She toiled tirelessly for my sake and endured countless hardships, yet she never gave up.It is solely because of her hard work, love, and the values she instilled in me that I am able to move forward in life today.My mother's struggles, her love, and her sacrifices hold immense significance for me.God has blessed me with the strongest and most loving mother in the world. So, thank you, God, for giving me this mother.",
+      "My Mother Name is Megha Chhagan Patil. Ever since my father passed away, my mother has single-handedly shouldered the entire responsibility of our home.She never once let me feel the absence of my father.My mother raised me by showering me with the love of both a mother and a father.She toiled tirelessly for my sake and endured countless hardships, yet she never gave up.It is solely because of her hard work, love, and the values she instilled in me that I am able to move forward in life today.My mother's struggles, her love, and her sacrifices hold immense significance for me.God has blessed me with the strongest and most loving mother in the world. So, thank you, God, for giving me this mother.",
     techStack: ["My Mother is MY Wold "],
     image: "assets/family/family3.webp",
   },
@@ -32,7 +32,7 @@ const familylist = [
     number: "04",
     title: "It's My Sister",
     description:
-      "My sister has always stood by me like a pillar of support.After our father passed away, she, alongside our mother, shouldered a great deal of responsibility.She always looks out for me, offers guidance, and stands by my side at every step.To me, my sister is not merely a sibling, but also a close friend and a source of inspiration.Her love, her support, and her affection are deeply precious to me.",
+      "My Sister Name is Shruti Chhagan Patil. My sister has always stood by me like a pillar of support.After our father passed away, she, alongside our mother, shouldered a great deal of responsibility.She always looks out for me, offers guidance, and stands by my side at every step.To me, my sister is not merely a sibling, but also a close friend and a source of inspiration.Her love, her support, and her affection are deeply precious to me.",
     techStack: ["My Cute Sister"],
     image: "assets/family/family4.webp",
   },
